d4bd9716b9d78410d5a6bb4b6bbd6e2bd6a105fc
Arya Wiratama <aryawiratama2401@gmail.com>
2025-04-22 21:43:36 +0700

    web rillchat

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// const sidebar = document.getElementById("sidebar");
+// const overlay = document.getElementById("overlay");
+// const menuBtn = document.getElementById("menu-btn");
+// const closeBtn = document.getElementById("close-btn");
+
+// menuBtn.addEventListener("click", () => {
+//   sidebar.classList.add("open");
+//   overlay.classList.add("active");
+// });
+
+// closeBtn.addEventListener("click", () => {
+//   sidebar.classList.remove("open");
+//   overlay.classList.remove("active");
+// });
+
+// overlay.addEventListener("click", () => {
+//   sidebar.classList.remove("open");
+//   overlay.classList.remove("active");
+// });
