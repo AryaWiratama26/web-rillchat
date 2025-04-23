19c9f39247287dd9502b76c7dc5a4065a6d5ee79
Arya Wiratama <aryawiratama2401@gmail.com>
2025-04-23 11:43:58 +0700

    styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,29 @@
-// const sidebar = document.getElementById("sidebar");
-// const overlay = document.getElementById("overlay");
-// const menuBtn = document.getElementById("menu-btn");
-// const closeBtn = document.getElementById("close-btn");
+document.addEventListener('DOMContentLoaded', function () {
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('overlay');
+    const menuBtn = document.getElementById('menu-btn');
+    const closeBtn = document.getElementById('close-btn');
+    const darkToggle = document.getElementById('darkToggle');
+  
+    menuBtn.onclick = () => {
+      sidebar.classList.add('active');
+      overlay.classList.add('active');
+    };
+    closeBtn.onclick = () => {
+      sidebar.classList.remove('active');
+      overlay.classList.remove('active');
+    };
+    overlay.onclick = () => {
+      sidebar.classList.remove('active');
+      overlay.classList.remove('active');
+    };
+  
+    // Dark Mode
+    darkToggle.onclick = () => {
+      document.body.classList.toggle('dark');
+      const icon = darkToggle.querySelector('i');
+      icon.classList.toggle('uil-moon');
+      icon.classList.toggle('uil-sun');
+    };
+  });
 
-// menuBtn.addEventListener("click", () => {
-//   sidebar.classList.add("open");
-//   overlay.classList.add("active");
-// });
-
-// closeBtn.addEventListener("click", () => {
-//   sidebar.classList.remove("open");
-//   overlay.classList.remove("active");
-// });
-
-// overlay.addEventListener("click", () => {
-//   sidebar.classList.remove("open");
-//   overlay.classList.remove("active");
-// });
